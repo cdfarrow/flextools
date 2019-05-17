2449d936d661ae37a109944df8c6179cdffea91b
cdfarrow <flextoolshelp@gmail.com>
2019-05-17 15:38:05 +1200

    Add Chinese utility files

---
+++ FlexTools/Modules/Chinese/Utilities/check_oldsort_vs_pkl sort.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+#   check_dict_vs_sort
+#
+#   June 2011
+#
+#   Loads the Chinese database and checks all Hanzi vs Pinyin
+#   using the Sort string database to check for consistency between
+#   the two databases.
+
+import sys, codecs
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
+
+import site
+site.addsitedir("..\Lib")
+
+from ChineseUtilities import ChineseDB, SortStringDB
+import datafiles, os
+
+# ---------------------------------------------------------------
+
+# ---------------------------------------------------------------
+# Checking for Sort File covering all of XD dictionary
+
+OldSortDB = SortStringDB(os.path.join(datafiles.datapath, r"Archive\ch2sort_2004(utf8).txt"))
+SortDB = SortStringDB()
+
+print "Checking %s against sort file %s" % (OldSortDB.FileName, SortDB.FileName)
+
+notOk = missingComposed = 0
+
+for e in sorted(OldSortDB.items()):
+##    print e
+    try:
+        sort = SortDB[e[0]]
+    except KeyError:
+        print "Not in latest DB:", repr(e[0])
+        continue
+        
+    if sort <> e[1]:
+        if len(sort) < len(e[1]):
+            notOk += 1
+            print e[0], sort.keys(), "<>", e[1].keys()
+
+
+for i in [u'\u602b', u'\u602b\u7136']: print i
+
+##print "Dictionary entries =", len(HZdict)
+##print "Sort key entries =", len(SortDB)
+##print "\tMissing composed characters (ignored) =", missingComposed
+##print "\tKnown length mismatches (ignored) =", len(IgnoreErrors)
+##print
+print "\tUnknown errors =", notOk
+##
+##
---
+++ FlexTools/Modules/Chinese/Utilities/edit_sort_pickle.py
@@ -0,0 +1,44 @@
+#
+#   Interactive script for editing the char_data pickle
+#
+
+import site
+site.addsitedir("..\Lib")
+
+import datafiles
+
+
+SortData = datafiles.loadSortData()
+
+def sort_pinyin():
+    for x in SortData.values():
+        if x[1] <> sorted(x[1]):
+            print "Sorting Pinyin:", x
+            SortData[x[0]] = (x[0], sorted(x[1]), x[2], x[3])
+
+def save():
+    sort_pinyin()
+    datafiles.saveSortData( SortData )
+
+count = 0
+def add_py(key, py):
+    global count
+    z = SortData[key]
+    if py not in z[1]:
+        z[1].append(unicode(py))
+        SortData[key] = z
+        print key, z
+        count += 1
+    else:
+        pass #print "Duplicate Pinyin!"
+
+    
+print "Edit Chinese Sort Data"
+print "[%s]" % datafiles.SortPickle
+print "%d entries" % len(SortData)
+print
+print "Lookup with: SortData[u'\u5b50']"
+print "\tEntries are tuple: (Chinese, [Pinyin], Stroke count, Strokes)"
+print "\tE.g.", SortData[u'\u5b50']
+print "Add a valid Pinyin pronunciation with: add_py(u'\u5b50', 'pin1')"
+print "Save data with save()"
