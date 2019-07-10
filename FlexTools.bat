6bce007ff99dd2c2ea8e3cb3ad3e257fc1304360
cdfarrow <flextoolshelp@gmail.com>
2019-07-10 17:09:15 +1200

    Added comment

--- FlexTools.bat
+++ FlexTools.bat
@@ -2,6 +2,9 @@
 
 pushd FlexTools
 
+REM Assume that the default Python will match FLEx for 32/64 bit.
+REM If it doesn't, then the user will need to install the correct Python.
+
 REM Try py -2 to run Python 2.7
 py -2 FlexTools.py >..\error.log %* && goto END
 
