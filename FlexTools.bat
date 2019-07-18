8bd7a2c29287c9f99661e92a8716fbecc3c9f2ea
cdfarrow <flextoolshelp@gmail.com>
2019-07-18 14:38:29 +1200

    Changed to run from top-level folder
    This will put logs/error files in the top-level where they are easier to find.

--- FlexTools.bat
+++ FlexTools.bat
@@ -1,16 +1,12 @@
 @echo off
 
-pushd FlexTools
-
 REM Assume that the default Python will match FLEx for 32/64 bit.
 REM If it doesn't, then the user will need to install the correct Python.
 
-REM Try py -2 to run Python 2.7
-py -2 FlexTools.py >..\error.log %* && goto END
+REM Try 'py -2' to run Python 2.7
+py -2 FlexTools\FlexTools.py >..\error.log %* && goto END
 
-REM If that failed, try "python"
-python FlexTools.py >..\error.log %* && goto END
+REM If that failed, try 'python'
+python FlexTools\FlexTools.py >..\error.log %* && goto END
 
 :END
-popd
-REM notepad error.log
