03232258736aea9eb5a4bae8ab47d7ef8f2ccab6
cdfarrow <cdfarrow@users.noreply.github.com>
2022-05-27 15:38:13 +1200

    Bump supported version to 9.1.11

--- FlexTools/Version.py
+++ FlexTools/Version.py
@@ -9,4 +9,4 @@ number = "2.0.0 Beta5"
 # Minimum and maximum supported versions of Fieldworks
 # (Later versions should work if the LCM interface hasn't changed.)
 MinFWVersion = "9.0.4"
-MaxFWVersion = "9.1.9"
+MaxFWVersion = "9.1.11"
