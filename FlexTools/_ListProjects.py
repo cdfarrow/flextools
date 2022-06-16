f4a599c315b04b4c54e713b67b9ec93a70cf8a08
cdfarrow <cdfarrow@users.noreply.github.com>
2022-06-16 13:42:10 +1200

    Create _ListProjects.py
    Convenience script to get the Fieldworks project names.

---
+++ FlexTools/_ListProjects.py
@@ -0,0 +1,19 @@
+#
+#   _ListProjects
+#
+#   List the Fieldworks projects on this system.
+#
+
+from flexlibs import FLExInitialize, FLExCleanup
+from flexlibs import AllProjectNames
+
+#----------------------------------------------------------------
+
+
+if __name__ == "__main__":
+
+    FLExInitialize()
+
+    print("\n".join(AllProjectNames()))
+    
+    FLExCleanup()
