8c5aaf35f55e5231745c2a8129f2ed7071303415
cdfarrow <cdfarrow@users.noreply.github.com>
2022-06-16 12:50:03 +1200

    Added new modules

---
+++ FlexTools/Modules/Integrity Checks/Check_Count_of_Lexemes_in_Corpus.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+#
+#   Check_Count_of_Lexemes_in_Corpus
+#    - A FlexTools Module -
+#
+#   Uses two different methods to calculate the number of occurences of each
+#   lexeme in the corpus. Produces a report on any inconsistencies between
+#   the two calculations, which may indicate corrupted data, or an incomplete
+#   calculation algorithm.
+#
+#   C D Farrow
+#   May 2014
+#
+#   Platforms: Python.NET and IronPython
+#
+
+from __future__ import unicode_literals
+
+from FTModuleClass import *
+from SIL.LCModel import *
+
+from collections import defaultdict
+
+
+#----------------------------------------------------------------
+# Documentation that the user sees:
+
+docs = {FTM_Name       : "Check Count of Lexemes in Corpus",
+        FTM_Version    : 1,
+        FTM_ModifiesDB : False,
+        FTM_Synopsis   : "Check consistency of internal links between lexemes and texts.",
+        FTM_Help       : None,
+        FTM_Description:
+"""
+This module uses two different methods to calculate the number of 
+occurences of each lexeme in the corpus. Produces a report on any
+inconsistencies between the two calculations, which may indicate 
+corrupted data, or an incomplete calculation algorithm.
+"""
+}
+                 
+#----------------------------------------------------------------
+def CheckNumberInCorpus(project, report, modifyAllowed=False):
+
+    # First record lexeme uses from the texts.
+
+    occurrenceCount = defaultdict(int)
+    for seg in project.ObjectsIn(ISegmentRepository):
+        for analysis in seg.AnalysesRS:
+            if analysis.Analysis:
+                for mb in analysis.Analysis.MorphBundlesOS:
+                    # MorphRA links to the variant entry not the main entry
+                    # which is what we want to count here.
+                    # (SenseRA links to the main entry.)
+                    if mb.MorphRA:
+                        occurrenceCount[mb.MorphRA.Owner.Guid] += 1
+
+    # Then compare with EntryAnalysesCount()
+    
+    for entry in project.LexiconAllEntries():
+        lexeme = project.LexiconGetLexemeForm(entry)
+        count = project.LexiconEntryAnalysesCount(entry)
+        if occurrenceCount[entry.Guid] < count:
+            report.Warning("%s: Texts (%i) < EntryAnalysesCount (%i)"
+                        % (lexeme, occurrenceCount[entry.Guid], count))
+        elif occurrenceCount[entry.Guid] != count:
+            report.Info("%s: Texts (%i) != EntryAnalysesCount (%i)"
+                        % (lexeme, occurrenceCount[entry.Guid], count))
+
+
+#----------------------------------------------------------------
+# The name 'FlexToolsModule' must be defined like this:
+
+FlexToolsModule = FlexToolsModuleClass(runFunction = CheckNumberInCorpus,
+                                       docs = docs)
+            
+
+#----------------------------------------------------------------
+if __name__ == '__main__':
+    FlexToolsModule.Help()
---
+++ FlexTools/Modules/Reports/Incomplete_Analyses.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+#   Incomplete_Analyses
+#    - A FlexTools Module -
+#
+#   C D Farrow
+#   June 2014
+#
+#   Platforms: Python.NET and IronPython
+#
+
+from __future__ import unicode_literals
+
+from FTModuleClass import *
+from SIL.LCModel import *
+from SIL.LCModel.Core.KernelInterfaces import ITsString, ITsStrBldr
+
+from collections import defaultdict
+
+
+#----------------------------------------------------------------
+# Documentation that the user sees:
+
+docs = {FTM_Name       : "Incomplete Analyses",
+        FTM_Version    : 1,
+        FTM_ModifiesDB : False,
+        FTM_Synopsis   : "Report on analyses that have missing Morphs or Senses.",
+        FTM_Help       : None,
+        FTM_Description:
+"""
+This module reports all the words in the corpus that haven't been
+fully analysed, so the user can easily find gaps in their analysis.
+"""
+}
+                 
+#----------------------------------------------------------------
+def FTModule(project, report, modifyAllowed=False):
+
+    for seg in project.ObjectsIn(ISegmentRepository):
+        for analysis in seg.AnalysesRS:
+            if analysis.Analysis:
+                for mb in analysis.Analysis.MorphBundlesOS:
+                    if not mb.MorphRA or not mb.SenseRA:
+                        report.Warning("%s: Missing morphs or senses"
+                                       % ITsString(mb.Owner.ChooserNameTS).Text,
+                                       project.BuildGotoURL(analysis))
+
+#----------------------------------------------------------------
+# The name 'FlexToolsModule' must be defined like this:
+
+FlexToolsModule = FlexToolsModuleClass(runFunction = FTModule,
+                                       docs = docs)
+            
+
+#----------------------------------------------------------------
+if __name__ == '__main__':
+    FlexToolsModule.Help()
