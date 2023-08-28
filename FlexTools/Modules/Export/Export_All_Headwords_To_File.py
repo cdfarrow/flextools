276ffc84cdd909d7df30abce8655da626bfd5d9d
cdfarrow <cdfarrow@users.noreply.github.com>
2023-08-28 16:58:10 +1100

    Renamed Dump_* modules to Export_*

--- FlexTools/Modules/Export/Export_All_Headwords_To_File.py
+++ FlexTools/Modules/Export/Export_All_Headwords_To_File.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-#   Reports.Dump_Published_Headwords_To_File
+#   Export.Export_All_Headwords_To_File
 #    - A FlexTools Module -
 #
-#   Dump all Headwords in a FLEx project to a file.
+#   Export all Headwords in a FLEx project to a file.
 #
 #   Kien-Wei Tseng
 #   April 2016
@@ -17,14 +17,13 @@ import io
 #----------------------------------------------------------------
 # Documentation that the user sees:
 
-docs = {FTM_Name        : "Dump Published Headwords To File",
+docs = {FTM_Name        : "Export All Headwords To File",
         FTM_Version     : 1,
         FTM_ModifiesDB  : False,
-        FTM_Synopsis    : "Dump published Headwords to file.",
+        FTM_Synopsis    : "Export all headwords to file.",
         FTM_Description :
 """
-Dump all headwords to a file if they are marked to be published (PublishIn field has at least
-one value.)
+Export all Headwords to a file.
 """ }
                  
 #----------------------------------------------------------------
@@ -32,18 +31,18 @@ one value.)
 
 def MainFunction(project, report, modifyAllowed):
 
-    headwordsFile = "Headwords_Published_{0}.txt".format(project.ProjectName())
+    headwordsFile = "Headwords_All_{0}.txt".format(project.ProjectName())
     output = io.open(headwordsFile, mode="w", encoding="utf-8")
     headwords = []
     for e in project.LexiconAllEntries():
-        if project.LexiconGetPublishInCount(e) > 0:
-            headwords.append(project.LexiconGetHeadword(e))
+        headwords.append(project.LexiconGetHeadword(e))
+    
     numHeadwords = 0
     for headword in sorted(headwords, key=lambda s: s.lower()):
         output.write(headword + '\n')
         numHeadwords += 1
-    report.Info("Dumped {0} headwords to file {1}".format(
-                 numHeadwords, headwordsFile))
+    report.Info("Exported {0} headwords to file {1}".format(
+                numHeadwords, headwordsFile))
     report.Info("Total Lexical Entries in Project = {}".format(
                 project.LexiconNumberOfEntries()))
     output.close()		
