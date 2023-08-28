77bae421e2c99c2b8daf63e2f5da5c70a6523e59
cdfarrow <cdfarrow@users.noreply.github.com>
2023-08-28 17:30:11 +1100

    Update check_pinyin.py
    Remove Py 2 imports

--- FlexTools/Modules/Chinese/Lib/check_pinyin.py
+++ FlexTools/Modules/Chinese/Lib/check_pinyin.py
@@ -21,9 +21,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from __future__ import unicode_literals
-from builtins import str
-
 import segmenter
 import chin_utils
 import re
