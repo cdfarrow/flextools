329191cca1e98f42acc7f4c632255639f65e1146
cdfarrow <cdfarrow@users.noreply.github.com>
2024-04-17 17:08:51 +1000

    Update __init__.py
    Interim release date

--- flextoolslib/__init__.py
+++ flextoolslib/__init__.py
@@ -5,7 +5,7 @@
 #----------------------------------------------------------------------------
 
 # We use the date of release for the version number: YYYY.MM.DD
-version = "2024.3.13"
+version = "2024.4.17"
 
 # Minimum and maximum supported versions of Fieldworks
 # (Later versions should work if the LCM interface hasn't changed.)
