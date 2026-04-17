4bd860a167c639a35f425f4d0d997eca1886b332
cdfarrow <cdfarrow@users.noreply.github.com>
2026-04-17 17:09:00 +1100

    Confirmed compatibility with FW 9.3.8

--- flextoolslib/__init__.py
+++ flextoolslib/__init__.py
@@ -14,7 +14,7 @@ version = "2026.3.24b"
 # Minimum and maximum supported versions of Fieldworks
 # (Later versions should work if the LCM interface hasn't changed.)
 MinFWVersion = "9.0.17"
-MaxFWVersion = "9.3.7"
+MaxFWVersion = "9.3.8"
 
 
 #----------------------------------------------------------------------------
